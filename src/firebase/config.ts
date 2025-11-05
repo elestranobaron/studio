2260e04d6e0c5dc158c8038432d82e4f42148c18
Thomas Lloancy <thomaslloancy@gmail.com>
2025-11-05 11:55:47 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9534743514-17d90",
+  "appId": "1:364656552077:web:aa40632a193739b0915ac4",
+  "apiKey": "AIzaSyDUTyFRM9fOQqNt0yFwmxvN2g53efxCN4w",
+  "authDomain": "studio-9534743514-17d90.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "364656552077"
+};
