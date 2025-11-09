51a112c8d1f7189b710d62c279bd89a90b886f87
Thomas Lloancy <thomaslloancy@gmail.com>
2025-11-09 02:18:14 +0000

    il a carrément disparu la

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -4,15 +4,13 @@ import Image from "next/image";
 
 export function Logo({ className, showText = true }: { className?: string; showText?: boolean }) {
   return (
-    <div className={cn("flex items-center gap-2", className)}>
-      <div className="relative h-full w-full">
+    <div className={cn("relative flex items-center gap-2", className)}>
         <Image 
           src="/wodburner1.jpg" 
           alt="WODBurner Logo" 
           fill
           className="object-contain"
         />
-      </div>
       {showText && (
         <span className="text-xl font-bold font-headline text-foreground group-data-[collapsible=icon]:hidden">
           WODBurner
