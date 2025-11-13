4c40da1ce525d53c91c35ac1d36bc767c8c15eae
Thomas Lloancy <thomaslloancy@gmail.com>
2025-11-13 01:00:00 +0000

    tu peux mettre a jour le sitemap si il y a besoin

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,24 +4,25 @@ import { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://wodburner.app';
 
-  return [
-    {
-      url: baseUrl,
-      lastModified: new Date(),
-      changeFrequency: 'yearly',
-      priority: 1,
-    },
-    {
-      url: `${baseUrl}/login`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
-      url: `${baseUrl}/scan`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.5,
-    },
+  const routes = [
+    '', 
+    '/login', 
+    '/scan', 
+    '/generate', 
+    '/timers', 
+    '/timers/for-time',
+    '/timers/amrap',
+    '/timers/emom',
+    '/timers/tabata',
+    '/hero-wods', 
+    '/premium'
   ];
+
+  return routes.map((route) => ({
+    url: `${baseUrl}${route}`,
+    lastModified: new Date(),
+    // Priorities can be adjusted based on importance
+    priority: route === '' ? 1 : 0.8,
+    changeFrequency: route === '' ? 'daily' : 'monthly',
+  }));
 }
