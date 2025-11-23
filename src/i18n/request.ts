083d26d16e1b25e9823a4315f35f9dc3d9fe2607
Thomas Lloancy <thomaslloancy@gmail.com>
2025-11-23 01:58:15 +0000

    ok

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,7 +1,7 @@
 // src/i18n/request.ts
 import { getRequestConfig } from 'next-intl/server';
 
-// Ce fichier est OBLIGATOIRE avec next-intl ≥ 3.22
 export default getRequestConfig(async ({ locale }) => ({
-  messages: (await import(`../../messages/${locale}.json`)).default
+  locale: locale ?? 'en', // fallback si jamais undefined
+  messages: (await import(`../../messages/${locale ?? 'en'}.json`)).default,
 }));
