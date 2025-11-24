a529327c973217460c1f17adf1c8ae07b9ba4185
Thomas Lloancy <thomaslloancy@gmail.com>
2025-11-24 00:12:52 +0000

    je comprends pas -------------------------------------------------------

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,10 @@
-
 // src/app/layout.tsx
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages, getLocale } from 'next-intl/server';
 import { Toaster } from "@/components/ui/toaster";
 import { FirebaseProvider } from "@/firebase/provider";
 import "@/app/globals.css";
-import { defaultLocale } from '../../next-intl.config';
+import { defaultLocale } from '@/../next-intl.config';
 
 export const metadata = {
   title: "WODBurner",
