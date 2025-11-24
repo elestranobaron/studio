946db307d9e7a58c5506eef7033bf9c84b501373
Thomas Lloancy <thomaslloancy@gmail.com>
2025-11-24 01:01:11 +0000

    DEFAUTLY LOCALE VERS LE REPERTOIRE COURANT TI COMPREND OU PAS

--- workspace/src/middleware.ts
+++ workspace/src/middleware.ts
@@ -1,5 +1,5 @@
 import createMiddleware from 'next-intl/middleware';
-import { locales, defaultLocale, localePrefix } from '../next-intl.config';
+import { locales, defaultLocale, localePrefix } from './next-intl.config';
  
 export default createMiddleware({
   // A list of all locales that are supported
