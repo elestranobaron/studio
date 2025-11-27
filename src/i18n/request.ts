181388c824fdad40ed4462a4f35a551c68617c5a
Thomas Lloancy <thomaslloancy@gmail.com>
2025-11-27 15:57:55 +0000

    Argument of type '({ locale }: GetRequestConfigParams) => Promise<{ loca

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -2,8 +2,11 @@ import {getRequestConfig} from 'next-intl/server';
 import {routing} from './routing';
  
 export default getRequestConfig(async ({locale}) => {
-  // Validate that the incoming `locale` parameter is valid
-  if (!routing.locales.includes(locale as any)) locale = 'en';
+  // Validate that the incoming `locale` parameter is valid.
+  // If not, default to 'en'.
+  if (!routing.locales.includes(locale as any)) {
+    locale = routing.defaultLocale;
+  }
  
   return {
     locale,
