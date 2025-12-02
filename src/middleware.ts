fcb658c9a5929535d652f6f294a8bc2ab5bd4dd9
Thomas Lloancy <thomaslloancy@gmail.com>
2025-12-02 10:21:58 +0000

    pourquoi est ce que depuis peu le sitemap est devenu inaccessible Sitema

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,10 @@
+
 import createMiddleware from 'next-intl/middleware';
 import {routing} from './i18n/routing';
 
 export default createMiddleware(routing);
 
 export const config = {
-  // Matcher ignoring `/_next/` and `/api/` and video/image assets
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.(?:svg|png|jpg|jpeg|gif|webp|mp4|webm)$).*)']
+  // Matcher ignoring `/_next/`, `/api/` and assets like images, videos, and sitemaps.
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.(?:svg|png|jpg|jpeg|gif|webp|mp4|webm|xml)$).*)']
 };
