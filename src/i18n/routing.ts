722da2c8ba8a6eecc6e7f1826a9640f1b680a9d2
Thomas Lloancy <thomaslloancy@gmail.com>
2025-12-10 22:23:43 +0000

    === Node.js - Npm (google.nodejs.npm@1.1.0) === ***** CACHE MISS: "npm_m

--- src/i18n/routing.ts
+++
@@ -1,7 +0,0 @@
-import {defineRouting} from 'next-intl/routing';
-
-export const routing = defineRouting({
-  locales: ['en', 'fr', 'es', 'de', 'it', 'ca', 'pt', 'ru', 'ja', 'zh', 'pl', 'nl', 'ar', 'hi', 'ko', 'ro'],
-  defaultLocale: 'en',
-  localePrefix: 'as-needed'
-});
