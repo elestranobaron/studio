20052a772048bb038e4b7a60a200d169bb00384e
Thomas Lloancy <thomaslloancy@gmail.com>
2025-12-18 14:28:15 +0000

    pourquoi jai le lateral logo qui pete encore entryTypes longtask ignoré,

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,5 +8,6 @@ export default createMiddleware({
  
 export const config = {
   // Match only internationalized pathnames
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
+  // This regex excludes files with extensions (e.g. .mp4, .jpg) and API routes.
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|.*\\..*).*)']
 };
