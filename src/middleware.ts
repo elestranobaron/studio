52e791f4a50695310863d352050e1c804675bf76
Thomas Lloancy <thomaslloancy@gmail.com>
2025-12-21 18:44:20 +0000

    onsole ErrorServer Invalid i18n request configuration detected.
    Please

--- src/middleware.ts
+++
@@ -1,1 +0,0 @@
-// This file is being removed as it's being renamed to proxy.ts
--- src/next.config.js
+++ src/next.config.js
@@ -1,7 +1,7 @@
 // next.config.js
 const createNextIntlPlugin = require('next-intl/plugin');
 
-const withNextIntl = createNextIntlPlugin('./src/next-intl.config.ts');
+const withNextIntl = createNextIntlPlugin('./src/i18n.ts');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
