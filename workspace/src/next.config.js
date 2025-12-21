3d03e3cc009c56f97106eafb54f2634ca50fa4e3
Thomas Lloancy <thomaslloancy@gmail.com>
2025-12-21 19:33:09 +0000

    je tai donné la documentation de passage de next 15 a next 16 alors je r

---
+++ workspace/src/next.config.js
@@ -0,0 +1,35 @@
+// next.config.js
+const createNextIntlPlugin = require('next-intl/plugin');
+
+const withNextIntl = createNextIntlPlugin('./src/i18n.ts');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+module.exports = withNextIntl(nextConfig);
