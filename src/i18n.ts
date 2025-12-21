bb29647ae1c31fb275d3975fc3f3a2c5211e0344
Thomas Lloancy <thomaslloancy@gmail.com>
2025-12-21 19:11:39 +0000

    pourqioi il me fait cette erreur maintenant Creating an optimized produc

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,14 +1,13 @@
 import {getRequestConfig} from 'next-intl/server';
 import {notFound} from 'next/navigation';
- 
+
 const locales = ['en', 'fr', 'es', 'de', 'it', 'ca', 'pt', 'ru', 'ja', 'zh', 'pl', 'nl', 'ar', 'hi', 'ko', 'ro'];
- 
+
 export default getRequestConfig(async ({locale}) => {
-  // Validate that the incoming `locale` parameter is valid
-  if (!locales.includes(locale as any)) {
+  if (!locale || !locales.includes(locale)) {
     notFound();
   }
- 
+
   return {
     locale,
     messages: (await import(`../messages/${locale}.json`)).default
