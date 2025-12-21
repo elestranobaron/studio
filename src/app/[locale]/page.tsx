8de6042e20ac5ba62aae761ddae6438a41c4e21a
Thomas Lloancy <thomaslloancy@gmail.com>
2025-12-21 19:20:39 +0000

    attend parocntre pourquoi on a regresxe la 404 sur la page d accueil
    je

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,7 +1,9 @@
 import { redirect } from 'next/navigation';
-
+ 
 // This page only redirects to the dashboard.
 // The real "root" page is the dashboard.
-export default function RootPage() {
-  redirect('/dashboard');
+export default function RootPage({params: {locale}}: {params: {locale: string}}) {
+  // We manually build the path with the current locale
+  // because the redirect function doesn't automatically handle it.
+  redirect(`/${locale}/dashboard`);
 }
