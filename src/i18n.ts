7348be9cac726ac7d9442083d1726408a4316c17
Thomas Lloancy <thomaslloancy@gmail.com>
2026-03-14 05:55:17 +0000

    seo + locales patth ok

--- src/i18n.ts
+++ src/i18n.ts
@@ -15,6 +15,6 @@ export default getRequestConfig(async ({ requestLocale }) => {
 
   return {
     locale,  // Obligatoire maintenant !
-    messages: (await import(`../messages/${locale}.json`)).default
+    messages: (await import(`./messages/${locale}.json`)).default
   };
 });
